e7e07cf978bb3fef6f229308915c91a1f7ff01f4
Neel Mehta <neelmehta96@gmail.com>
2019-02-20 01:35:15 +0000

    build out basics of page

--- assets/js/forgot.js
+++ assets/js/forgot.js
@@ -3,17 +3,24 @@ $(function(){
         var email = "pdesai@spectacular.com";
 
         if ($('#forgot-email').val() === email) {
-            $('#login-button').removeClass('disabled').removeAttr('disabled').attr('href', 'recover.html');
+            // correct email
+            $('#login-button').removeClass('disabled').removeAttr('disabled').attr('href', 'recover.html')
+                .removeClass('btn-secondary').addClass('btn-success');
             $('#incorrect-email').hide();
             $('#correct-email').show();
         }
         else {
-            $('#login-button').addClass('disabled').attr('disabled', true);
+            // wrong email
+            $('#login-button').addClass('disabled').attr('disabled', true)
+                .addClass('btn-secondary').removeClass('btn-success');
+
             if ($('#forgot-email').val() == "") {
+                // cleared text box
                 $('#incorrect-email').hide();
                 $('#correct-email').hide();
             }
             else {
+                // wrong text entered
                 $('#incorrect-email').show();
                 $('#correct-email').hide();
             }
